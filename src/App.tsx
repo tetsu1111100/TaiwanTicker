3e8fd6d601a2f6f7ec2501f50c0b7c14a9df2871
DESKTOP-SA707TD\user <tetsu1111100@gmail.com>
2025-10-31 23:51:26 +0800

    feat: react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import { AiOutlineStock } from "react-icons/ai";
 
 function App() { 
 
   return (
     <>
-      <p className=" text-red-500 ">Hello world</p>
+      <p className=" text-red-500 ">Hello world <AiOutlineStock /></p>
     </>
   )
 }
